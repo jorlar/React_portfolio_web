a405b71110ac2248a193b6750603c800ecf14d97
Jørn Larsen <jorn.larsen@jotun-utvikling.no>
2022-07-31 13:55:23 +0200

    :sparkles: Add Sanity Dashboard to build

---
+++ frontend_react/dashboardConfig.js
@@ -0,0 +1,25 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'My Netlify deploys',
+          sites: [
+            {
+              title: 'Sanity Studio',
+              apiId: 'skX8XEb7zNuzYwLRePjhjDZd4jh9NXL8dZKzTlyd3MtEeHNNxpS5rfpEwwMYWytuOnWyMEfiX4NiV9DZl4bskGWEbzjWSFeSuUnYuy1aFJmnIAFmCqJKUzPNdnWaTngrSuFE7BVRn2nltUiJDdka1MT0L1D23N5pzA1Ovb4jtDpEe4e3IANE',
+              buildHookId: 'netlify',
+              name: 'sanity-gatsby-blog-20-studio',
+            },
+            {
+              title: 'Portoflio',
+              apiId: 'skX8XEb7zNuzYwLRePjhjDZd4jh9NXL8dZKzTlyd3MtEeHNNxpS5rfpEwwMYWytuOnWyMEfiX4NiV9DZl4bskGWEbzjWSFeSuUnYuy1aFJmnIAFmCqJKUzPNdnWaTngrSuFE7BVRn2nltUiJDdka1MT0L1D23N5pzA1Ovb4jtDpEe4e3IANE',
+              buildHookId: 'netlify',
+              name: 'sanity-gatsby-blog-20-web',
+              url: 'https://jornlarsen-portfolio.netlify.app/',
+            }
+          ]
+        }
+      }
+    ]
+  }
